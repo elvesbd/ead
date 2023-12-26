5cc30b86f683a549ff196e4e65cb1dfac871da98
elvesbd <elvesbd@yahoo.com>
2023-12-26 19:39:05 -0300

    chore: remove property

--- src/shared/value-objects/Id.ts
+++ src/shared/value-objects/Id.ts
@@ -4,18 +4,12 @@ import { v4 as uuid, validate } from "uuid";
 
 export default class Id {
   readonly value: string;
-  readonly new: boolean;
 
   constructor(value?: string) {
     this.value = value ?? uuid();
-    this.new = !value;
 
     if (!validate(this.value))
       ErrorValidation.throw(ErrorType.INVALID_ID, this.value);
-  }
-
-  static get new() {
-    return new Id();
   }
 
   isEqual(id: Id) {
