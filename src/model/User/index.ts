e3aa1cf5326a66910fa3642b4e5dc39faa92a3a0
elvesbd <elvesbd@yahoo.com>
2024-01-05 15:45:38 -0300

    chore: adjusting validations

--- src/model/User/index.ts
+++ src/model/User/index.ts
@@ -2,20 +2,33 @@ import { UserProps } from './types/UserProps';
 import Email from '@/shared/ValueObjects/Email';
 import Entity from '@/shared/ValueObjects/Entity';
 import PersonName from '@/shared/ValueObjects/PersonName';
-import PasswordStrong from '@/shared/ValueObjects/PasswordStrong';
+import PasswordHash from '@/shared/ValueObjects/PasswordHash';
+import Validator from '@/utils/Validator';
+import { ValidatorOutput } from '@/utils/Validator/types/Validator';
 
 export default class User extends Entity<User, UserProps> {
   private readonly email: Email;
   private readonly name: PersonName;
-  private readonly password: PasswordStrong;
+  private readonly password: PasswordHash;
 
   constructor(props: UserProps) {
     super(props);
+
     this.name = new PersonName({
       firstName: props.firstName,
       lastName: props.lastName,
     });
     this.email = new Email(props.email);
-    this.password = new PasswordStrong(props.password);
+    this.password = new PasswordHash(props.password);
+  }
+
+  validate(): ValidatorOutput {
+    const validation = new Validator();
+
+    this.name.validate(validation);
+    this.email.validate(validation);
+    this.password.validate(validation);
+
+    return validation.getOutput();
   }
 }
