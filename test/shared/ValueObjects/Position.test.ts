d20b54c82c46e027086341408064f0fbc22f5244
elvesbd <elvesbd@yahoo.com>
2024-01-21 12:21:21 -0300

    test: added unit test for sort method

--- test/shared/ValueObjects/Position.test.ts
+++ test/shared/ValueObjects/Position.test.ts
@@ -24,59 +24,74 @@ describe('Value Object - Position', () => {
   describe('Methods', () => {
     describe('isEqual()', () => {
       it('should return true if positions equals', () => {
-        const newPosition = new Position(5);
-        expect(position.isEqual(newPosition)).toBeTruthy();
+        const otherPosition = new Position(5);
+        expect(position.isEqual(otherPosition)).toBeTruthy();
       });
 
       it('should return false if positions equals', () => {
-        const newPosition = new Position(2);
-        expect(position.isEqual(newPosition)).toBeFalsy();
+        const otherPosition = new Position(2);
+        expect(position.isEqual(otherPosition)).toBeFalsy();
       });
     });
 
     describe('isDifferent()', () => {
       it('should return true if positions is equals', () => {
-        const newPosition = new Position(6);
+        const otherPosition = new Position(6);
 
-        expect(position.isDifferent(newPosition)).toBeTruthy();
+        expect(position.isDifferent(otherPosition)).toBeTruthy();
       });
 
       it('should return false if positions is not equals', () => {
-        const newPosition = new Position(5);
+        const otherPosition = new Position(5);
 
-        expect(position.isDifferent(newPosition)).toBeFalsy();
+        expect(position.isDifferent(otherPosition)).toBeFalsy();
       });
     });
 
     describe('compare()', () => {
       it('should return 0 if positions is equals', () => {
-        const newPosition = new Position(5);
+        const otherPosition = new Position(5);
 
-        expect(position.compare(newPosition)).toBe(0);
+        expect(position.compare(otherPosition)).toBe(0);
       });
 
       it('should return 1 if current position is greater than the new position', () => {
-        const newPosition = new Position(2);
+        const otherPosition = new Position(2);
 
-        expect(position.compare(newPosition)).toBe(1);
+        expect(position.compare(otherPosition)).toBe(1);
       });
 
       it('should return -1 if current position is smaller than the new position', () => {
-        const newPosition = new Position(8);
+        const otherPosition = new Position(8);
 
-        expect(position.compare(newPosition)).toBe(-1);
+        expect(position.compare(otherPosition)).toBe(-1);
+      });
+    });
+
+    describe('sorting()', () => {
+      it('should return -1 if current position is smaller than the new position', () => {
+        const positions = [new Position(3), new Position(1), new Position(2)];
+        const expectedResult = [
+          new Position(1),
+          new Position(2),
+          new Position(3),
+        ];
+
+        const result = position.sorting(positions);
+
+        expect(result).toStrictEqual(expectedResult);
       });
     });
 
     describe('getNotifications()', () => {
       it('should return notification if value is zero', () => {
         const value = 0;
-        const position = new Position(value);
+        const otherPosition = new Position(value);
         const expectedResult = {
           Position: ['Position deve ser maior que 0!'],
         };
 
-        const result = position.getNotifications();
+        const result = otherPosition.getNotifications();
 
         expect(result).toStrictEqual(expectedResult);
       });
