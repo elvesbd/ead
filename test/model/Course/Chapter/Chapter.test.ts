980ff0d95553a086fe480f31e8799c5bad062960
elvesbd <elvesbd@yahoo.com>
2024-03-07 10:08:01 -0300

    test: return the correct order of the lessons

--- test/model/Course/Chapter/Chapter.test.ts
+++ test/model/Course/Chapter/Chapter.test.ts
@@ -21,7 +21,15 @@ describe('Entity - Chapter', () => {
   describe('getProps()', () => {
     it('should return chapter props on success', () => {
       const result = chapter.getProps();
-      expect(result).toStrictEqual(props);
+
+      expect(result).toHaveProperty('name');
+      expect(result).toHaveProperty('lessons');
+      expect(result).toHaveProperty('position');
+
+      expect(result.name).toBeDefined();
+      expect(result.lessons).toHaveLength(10);
+      expect(result.position).toBeGreaterThanOrEqual(1);
+      expect(result.position).toBeLessThanOrEqual(100);
     });
   });
 
@@ -102,6 +110,33 @@ describe('Entity - Chapter', () => {
   });
 
   describe('getDuration()', () => {
-    it('should return the correct order of the lessons', () => {});
+    it('should return the correct order of the lessons', () => {
+      const lessons = [
+        LessonBuilder.aLesson()
+          .withName('Aula #1')
+          .withDuration(63)
+          .withPosition(1)
+          .build(),
+
+        LessonBuilder.aLesson()
+          .withName('Aula #2')
+          .withDuration(1007)
+          .withPosition(1)
+          .build(),
+
+        LessonBuilder.aLesson()
+          .withName('Aula #3')
+          .withDuration(3784)
+          .withPosition(1)
+          .build(),
+      ];
+      const props = ChapterBuilder.aChapter().withLessons(lessons).build();
+
+      const chapter = new Chapter(props);
+
+      expect(chapter.lessons[0].position.value).toBe(1);
+      expect(chapter.lessons[1].position.value).toBe(2);
+      expect(chapter.lessons[2].position.value).toBe(3);
+    });
   });
 });
